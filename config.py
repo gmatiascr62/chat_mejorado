2bd6778f38763cbe049e48f5ae4bfd169a778357
matias <gmatiascr62@gmail.com>
2022-08-04 16:21:13 -0300

    saco postres

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     DEBUG = False
     SECRET_KEY = os.environ.get('secret')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
