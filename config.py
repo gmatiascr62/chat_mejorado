b6cc528c7b02fd6f5bf40a57bfc0be0cd3e694b3
matias <gmatiascr62@gmail.com>
2022-08-07 11:40:20 -0300

    nise

--- config.py
+++ config.py
@@ -4,11 +4,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     DEBUG = False
-<<<<<<< HEAD
-    SECRET_KEY = os.environ.get('secret')
-=======
     SECRET_KEY = 'secrtuky23876213et'
->>>>>>> b500ddd (probando)
     SQLALCHEMY_DATABASE_URI = 'postgresql://teqzadvo:bXM6zS-Vt-0Qzv-SvIHQa6YvUfNTRUD4@rosie.db.elephantsql.com/teqzadvo'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
